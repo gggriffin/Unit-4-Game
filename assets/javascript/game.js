be666ac3ffae5576d4fa4548b06d29e8effa749a
gggriffin <griffin.garrett@gmail.com>
2019-03-17 12:27:05 -0500

    final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,10 +17,8 @@ $(document).ready(function() {
         thirdCrystal = 1 + Math.floor(Math.random() * 12);
         fourthCrystal = 1 + Math.floor(Math.random() * 12);
 
-        console.log(targetNumber, 'target number upon reset');
         $("#targetNumber").text("Target Number: " + targetNumber);
-        console.log(targetNumber, 'target number after changing DOM');
-        $("#userScore").text("Your current score: " + userScore);
+        $("#userScore").text("Current Score: " + userScore);
         $("#wins").text("Wins: " + wins);
         $("#losses").text("Losses: " + losses);
 
@@ -34,106 +32,65 @@ $(document).ready(function() {
     $("#firstCrystal").click(function() {
 
         userScore = (firstCrystal + userScore);
-        $("#userScore").text("Your current score: " + userScore);
-        console.log("Before if/else score " + userScore);
-        console.log("Before if/else crystal " +firstCrystal);
+        $("#userScore").text("Current Score: " + userScore);
 
-        console.log(userScore, 'userScore');
-        console.log(targetNumber, 'targetNumber')
         if (userScore == targetNumber) {
             wins++;
             reset();
-            console.log("After if score " + userScore);
-            console.log("After if crystal " +firstCrystal);
             
         } else if (userScore > targetNumber) {
             losses++
             reset();
-            console.log("After else score " + userScore);
-            console.log("After else crystal " + firstCrystal);
         }
     });
 
     $("#secondCrystal").click(function() {
 
         userScore = (secondCrystal + userScore);
-        $("#userScore").text("Your current score: " + userScore);
-        console.log("Before if/else score " + userScore);
-        console.log("Before if/else crystal " +firstCrystal);
-
-        console.log(userScore, 'userScore');
-        console.log(targetNumber, 'targetNumber')
+        $("#userScore").text("Current Score: " + userScore);
+        
         if (userScore == targetNumber) {
             wins++;
             reset();
-            console.log("After if score " + userScore);
-            console.log("After if crystal " +firstCrystal);
             
         } else if (userScore > targetNumber) {
             losses++
             reset();
-            console.log("After else score " + userScore);
-            console.log("After else crystal " + firstCrystal);
         }
     });
 
     $("#thirdCrystal").click(function() {
 
         userScore = (thirdCrystal + userScore);
-        $("#userScore").text("Your current score: " + userScore);
-        console.log("Before if/else score " + userScore);
-        console.log("Before if/else crystal " +firstCrystal);
-
-        console.log(userScore, 'userScore');
-        console.log(targetNumber, 'targetNumber')
+        $("#userScore").text("Current Score: " + userScore);
+        
         if (userScore == targetNumber) {
             wins++;
             reset();
-            console.log("After if score " + userScore);
-            console.log("After if crystal " +firstCrystal);
             
         } else if (userScore > targetNumber) {
             losses++
             reset();
-            console.log("After else score " + userScore);
-            console.log("After else crystal " + firstCrystal);
         }
     });
 
     $("#fourthCrystal").click(function() {
 
         userScore = (fourthCrystal + userScore);
-        $("#userScore").text("Your current score: " + userScore);
-        console.log("Before if/else score " + userScore);
-        console.log("Before if/else crystal " +firstCrystal);
+        $("#userScore").text("Current Score: " + userScore);
 
-        console.log(userScore, 'userScore');
-        console.log(targetNumber, 'targetNumber')
         if (userScore == targetNumber) {
             wins++;
             reset();
-            console.log("After if score " + userScore);
-            console.log("After if crystal " +firstCrystal);
             
         } else if (userScore > targetNumber) {
             losses++
             reset();
-            console.log("After else score " + userScore);
-            console.log("After else crystal " + firstCrystal);
         }
     });
 
 });
 
-//values don't seem to be fully resetting. 
-//userScore is not resetting on reset. The if and the else
-//statements both run on every click after the first userScore == or is > target score
-
-//To do:
-//1. Crystal button press adds Crystal value to userScore, update userScore display
-
-//2a. if userScore == targetNumber, wins++, run reset()
-//2b. else if userScore > targetNumber, losses++, run reset()
 
 
 
